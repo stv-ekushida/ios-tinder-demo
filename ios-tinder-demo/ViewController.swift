702ffbb5a4689966c30967f1a5de381f27f3d46d
sdt-kushida <e.kushida@st-ventures.jp>
2018-01-06 08:10:44 +0900

    Initial Commit

---
+++ ios-tinder-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-tinder-demo
+//
+//  Created by Eiji Kushida on 2018/01/06.
+//  Copyright © 2018年 Eiji Kushida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios-tinder-demoTests/ios_tinder_demoTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_tinder_demoTests.swift
+//  ios-tinder-demoTests
+//
+//  Created by Eiji Kushida on 2018/01/06.
+//  Copyright © 2018年 Eiji Kushida. All rights reserved.
+//
+
+import XCTest
+@testable import ios_tinder_demo
+
+class ios_tinder_demoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
